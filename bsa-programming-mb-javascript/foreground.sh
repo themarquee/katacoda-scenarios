d2e8cc99a453ad2fc0e2826b64b2aa141fc18f0a
Marc Anderson <marc.n.anderson@gmail.com>
2022-02-11 02:16:18 -0600

    update JS kata

--- bsa-programming-mb-javascript/foreground.sh
+++ bsa-programming-mb-javascript/foreground.sh
@@ -1,7 +1,17 @@
 #/bin/bash
 
-printf 'This is a bash shell script.\n\n'
-printf 'Any commands you put in here will be executed at runtime.\n\n'
-printf 'As such, this is a good place to install and configure anything you want to have ready for the learner before they get started.\n\n'
-printf 'The file is named foreground.sh because when it executes, the commands are echoed to the terminal.\n\n'
-printf 'To run a script in the background, so learners will not see it execute, specify it in the index.json as "background": "background.sh"\n\n'
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
+gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+
+# The output should contain the full fingerprint 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62 as follows:
+#    pub   rsa2048 2011-08-19 [SC] [expires: 2024-06-14]
+#          573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62
+#    uid                      nginx signing key <signing-key@nginx.com>
+# If the fingerprint is different, remove the file.
+
+sudo apt update
+sudo apt install nginx
