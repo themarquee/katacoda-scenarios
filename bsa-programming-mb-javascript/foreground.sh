e44004778fe50b99492d6fc9a51acce012950af3
Marc Anderson <marc.n.anderson@gmail.com>
2022-02-11 03:47:29 -0600

    Update foreground.sh

--- bsa-programming-mb-javascript/foreground.sh
+++ bsa-programming-mb-javascript/foreground.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-sudo apt update
-sudo apt install nginx
+apt update
+apt install -y nginx
 
 # The output should contain the full fingerprint 573BFD6B3D8FBC641079A6ABABF5BD827BD9BF62 as follows:
 #    pub   rsa2048 2011-08-19 [SC] [expires: 2024-06-14]
