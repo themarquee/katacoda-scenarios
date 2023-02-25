a0a22b95e6a15c2ffdc70abc8a21e011ae82ec0b
Marc Anderson <marc.n.anderson@gmail.com>
2023-02-25 04:59:47 -0600

    Update foreground.sh

--- bsa-programming-java/foreground.sh
+++ bsa-programming-java/foreground.sh
@@ -1,5 +1,4 @@
 #echo waiting for step1-background-script to finish
 #while [ ! -f /tmp/background1 ]; do sleep 1; done
 #echo BACKGROUND DONE
-apt-get -y install zip && curl -s "https://get.sdkman.io" | bash && source "$HOME/.sdkman/bin/sdkman-init.sh" 
-sdk version && sdk install java && sdk install springboot
+apt-get -y install zip && curl -s "https://get.sdkman.io" | bash && source "$HOME/.sdkman/bin/sdkman-init.sh" && sdk version && sdk install java && sdk install springboot
